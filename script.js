c32b46d226de0d248c5200dd28a89859d2ad0a46
Sirisha <sirishakunduru26@gmail.com>
2025-11-17 16:11:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function beforesubmit() {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log(inputdate.value);
+
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+    outputdate.value = formattedDate;
+    console.log('outputdate.value',outputdate.value);
+}
